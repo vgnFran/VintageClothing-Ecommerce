2651a22fca2b687f6db4c265865072559ba6d8c2
vgnFran <francohoyos2112@gmail.com>
2022-12-26 18:21:34 -0300

    login terminado

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-// const todos= document.querySelector("#todos")
-
-// todos.addEventListener("click",()=>{
-//     alert("click")
-// })
-
-
 const registrarLogin = document.querySelector("#registro-login");
 const bodyEcomercce = document.querySelector("#ecomercce-body");
 const formLogin= document.querySelector("#registrar");
@@ -17,17 +10,26 @@ const passRegistrado= document.querySelector("#pass-registrado");
 const iniciarRegistrado= document.querySelector("#iniciar-registrado")
 const aIniciar= document.querySelector("#aIniciar");
 const cierraSesion= document.querySelector("#cierra-sesion")
+const userIncorrecto= document.querySelector("#incorrecto")
 
+
+// datos de usuarios guardados en "base de datos en js"
 let usuario1={
     usuario:"Franco",
     contrasenia:"123456"
 }
 
-
 const registrarLocalStorage = (clave,valor) =>{
     localStorage.setItem(clave,JSON.stringify(valor))
 }
 
+
+const comprobarLocalStorage = ( clave ) => {
+    return JSON.parse(localStorage.getItem(clave))
+}
+
+
+// funcion para registrar usuario nuevo (guardado en localStorage)
 formLogin.onsubmit= (evento)=>{
     evento.preventDefault()
     if(inputUsuario.value!="" && inputPass.value!=""){
@@ -44,26 +46,41 @@ formLogin.onsubmit= (evento)=>{
 }
 
 
+
+// funcion que nos muestra un login para usuarios ya registrados (en nuestra base de datos o en localStorage)
 aIniciar.onclick=(evento)=>{
     evento.preventDefault()
     registrarLogin.style.display="none";
-    iniciarRegistrado.style.display="flex"
+    iniciarRegistrado.style.display="flex";
+
     
 }
 
-cierraSesion.onclick= (evento)=>{
 
-}
-
+// funcion para iniciar sesion con usuarios ya registrados
 iniciaSesion.onsubmit=(evento)=>{
     evento.preventDefault()
-    if(userRegistrado.value==usuario1.usuario && passRegistrado.value==usuario1.contrasenia){
-        iniciarRegistrado.style.display="none"
-        bodyEcomercce.style.display="block"
-        nombreUser.innerText=userRegistrado.value
-    }else if(userRegistrado.value!= usuario1.usuario){
-        userRegistrado.style.border= "2px solid red"
-    }else if(passRegistrado.value != usuario1.contrasenia){
-        passRegistrado.style.border= "2px solid red"
+    if((comprobarLocalStorage(userRegistrado.value)==passRegistrado.value) || (userRegistrado.value == usuario1.usuario && passRegistrado.value ==usuario1.contrasenia ) ){
+        iniciarRegistrado.style.display="none";
+        bodyEcomercce.style.display="block";
+        nombreUser.innerText=userRegistrado.value;
+
+    }else if(comprobarLocalStorage(userRegistrado.value)!= passRegistrado.value ){
+        userIncorrecto.style.display= "block"
+        userRegistrado.style.border= "2px solid red";
+        passRegistrado.style.border= "2px solid red";
+
     }
+ }
+
+
+// funcion para cerrar sesion 
+ cierraSesion.onclick= (evento)=>{
+    evento.preventDefault();
+    bodyEcomercce.style.display="none";
+    registrarLogin.style.display="block";
+    userRegistrado.value="";
+    passRegistrado.value="";
+    inputUsuario.value="";
+    inputPass.value="";
 }
