b56bba922292c923415703c74cbcf1c9ba2ff01e
vgnFran <francohoyos2112@gmail.com>
2023-01-21 23:57:07 -0300

    limpieza de codigo

--- js/mainCarrito.js
+++ js/mainCarrito.js
@@ -1,23 +1,23 @@
-const nombreUser = document.querySelector("#nombre-user")
-const cantidadProductos= document.querySelector("#cantidad")
-const precioIndividual= document.querySelector("#precio-individual")
-const comprar= document.querySelector(".comprar")
-const cuantosProductos= document.querySelector(".span-carrito")
+const nombreUser = document.querySelector("#nombre-user");
+const cantidadProductos= document.querySelector("#cantidad");
+const precioIndividual= document.querySelector("#precio-individual");
+const comprar= document.querySelector(".comprar");
+const cuantosProductos= document.querySelector(".span-carrito");
 
-const totalFinal= document.querySelectorAll("#total")
-const interruptor= document.querySelector(".interruptor")
-const productosTitulo=document.querySelector("#todos-productos")
+const totalFinal= document.querySelectorAll("#total");
+const interruptor= document.querySelector(".interruptor");
+const productosTitulo=document.querySelector("#todos-productos");
 const aside=document.querySelector(".aside");
-const main=document.querySelector(".main")
+const main=document.querySelector(".main");
 const body=document.querySelector("body");
-const modoCheckbox= document.querySelector("#toggle")
-const contenedorJs= document.querySelector("#container-js")
-const vaciar= document.querySelector(".vaciar")
-const precioTotal = document.querySelector("#total-final")
+const modoCheckbox= document.querySelector("#toggle");
+const contenedorJs= document.querySelector("#container-js");
+const vaciar= document.querySelector(".vaciar");
+const precioTotal = document.querySelector("#total-final");
 
-const bodyCarrito= document.querySelector(".body-ecomercce")
-const bodyCard= document.querySelector(".container-card")
-const cerrar= document.querySelector("#close")
+const bodyCarrito= document.querySelector(".body-ecomercce");
+const bodyCard= document.querySelector(".container-card");
+const cerrar= document.querySelector("#close");
 
 const registrarLocalStorage = (clave,valor) =>{
     localStorage.setItem(clave,JSON.stringify(valor))
@@ -33,23 +33,23 @@ const comprobarLocalStorage = ( clave ) => {
 //validar nombre de usuario que inicio sesion
 const cargarCarrito=()=>{
     document.body.onload=()=>{
-        const nombre= comprobarLocalStorage("nombre")      
-        nombreUser.textContent= nombre
+        const nombre= comprobarLocalStorage("nombre");
+        nombreUser.textContent= nombre;
     }
 }
-cargarCarrito()
+cargarCarrito();
 
 
 
 // modo oscuro
 interruptor.onclick= ()=>{
-    productosTitulo.classList.toggle("modo-oscuro")
-    aside.classList.toggle("modo-oscuro")
-    main.classList.toggle("modo-oscuro-main")
-    body.classList.toggle("modo-oscuro")
-    registrarLocalStorage("modo","oscuro")
+    productosTitulo.classList.toggle("modo-oscuro");
+    aside.classList.toggle("modo-oscuro");
+    main.classList.toggle("modo-oscuro-main");
+    body.classList.toggle("modo-oscuro");
+    registrarLocalStorage("modo","oscuro");
     if(modoCheckbox.checked==true){
-        localStorage.removeItem("modo")
+        localStorage.removeItem("modo");
     }
 }
 
@@ -57,32 +57,32 @@ interruptor.onclick= ()=>{
 
 function validarModoOscuro(clave){
     if  (clave=="oscuro"){
-        productosTitulo.classList.add("modo-oscuro")
-        aside.classList.add("modo-oscuro")
-        main.classList.add("modo-oscuro-main")
-        body.classList.add("modo-oscuro")
-        modoCheckbox.checked=true
+        productosTitulo.classList.add("modo-oscuro");
+        aside.classList.add("modo-oscuro");
+        main.classList.add("modo-oscuro-main");
+        body.classList.add("modo-oscuro");
+        modoCheckbox.checked=true;
 
     }else {
-        productosTitulo.classList.remove("modo-oscuro")
-        aside.classList.remove("modo-oscuro")
-        main.classList.remove("modo-oscuro-main")
-        body.classList.remove("modo-oscuro")
-        modoCheckbox.checked=false
+        productosTitulo.classList.remove("modo-oscuro");
+        aside.classList.remove("modo-oscuro");
+        main.classList.remove("modo-oscuro-main");
+        body.classList.remove("modo-oscuro");
+        modoCheckbox.checked=false;
     }
 
 
 }
 
-validarModoOscuro(comprobarLocalStorage("modo"))
+validarModoOscuro(comprobarLocalStorage("modo"));
 
 //trayendo los productos del carrito subidos al ls
 
-productosCarro= JSON.parse(localStorage.getItem("carro"))
+productosCarro= JSON.parse(localStorage.getItem("carro"));
 
 
 
-let precioFinal=[] // variable para sumar el precio final sumando el precio de todos los productos
+let precioFinal=[]; // variable para sumar el precio final sumando el precio de todos los productos
 
 function agregar(arr){
     
@@ -100,35 +100,37 @@ function agregar(arr){
             <p id="precio-individual">${element.precio}</p>
         </div>
         `
-        contenedorJs.appendChild(contenedor)
-        precioFinal.push(element.precio)
+        contenedorJs.appendChild(contenedor);
+        precioFinal.push(element.precio);
     });
 }
-agregar(productosCarro || [])
+agregar(productosCarro || []);
 
+
+//funcion para borrar productos del carrito
 function borrarDelCarrito (array) {
-    const botonBorrar = document.querySelectorAll(".boton-eliminar")    
+    const botonBorrar = document.querySelectorAll(".boton-eliminar");    
     botonBorrar.forEach( boton => {
         boton.onclick = () => {
-            contenedorJs.innerHTML=""
-            const id = boton.id.slice(6)            
+            contenedorJs.innerHTML="";
+            const id = boton.id.slice(6);            
             const filtrarProducto = array.filter((elemento, i) => {
-                return elemento.id != Number(id)
+                return elemento.id != Number(id);
             })
-            productosCarro = filtrarProducto            
-            localStorage.setItem("carro", JSON.stringify(productosCarro))   
-            agregar(productosCarro)
-            borrarDelCarrito(productosCarro)          
-            registrarLocalStorage("cuantos",productosCarro.length)
-            sinProductos()
+            productosCarro = filtrarProducto;            
+            localStorage.setItem("carro", JSON.stringify(productosCarro));   
+            agregar(productosCarro);
+            borrarDelCarrito(productosCarro);          
+            registrarLocalStorage("cuantos",productosCarro.length);
+            sinProductos();
             //para obtener el precio final del carrito
             total= productosCarro.map(e=>{
-                const nada= 0
+                const nada= 0;
                 return nada + e.precio
             })
             let precioFinal = total.reduce((a, b) => a + b, 0);
-            precioTotal.innerText="Total: $"+precioFinal
-            comprobar()
+            precioTotal.innerText="Total: $"+precioFinal;
+            comprobar();
         }
         
     })
@@ -137,53 +139,53 @@ function borrarDelCarrito (array) {
 
 
 
-borrarDelCarrito(productosCarro)
+borrarDelCarrito(productosCarro);
 
 
 //eliminando todos los productos del carrito y del ls
 vaciar.onclick=()=>{
-    registrarLocalStorage("carro",[])
-    contenedorJs.innerHTML=""
-    localStorage.setItem("cuantos",0)
-    sinProductos()
+    registrarLocalStorage("carro",[]);
+    contenedorJs.innerHTML="";
+    localStorage.setItem("cuantos",0);
+    sinProductos();
 }
 
 
-
+// funcion para modificar el dom si es que no tenemos nada en el carrito
 const sinProductos=()=>{
     if (comprobarLocalStorage("carro").length==0  ){
-        const contenedor= document.createElement("div")
+        const contenedor= document.createElement("div");
         contenedor.className="productos-vacios"
         contenedor.innerHTML=`
         <p>El carrito de compras se encuentra vacio. <i class="bi bi-cart"></i>
         </p>
         `
-        contenedorJs.appendChild(contenedor)
-        precioTotal.innerText="Total: $"+0
+        contenedorJs.appendChild(contenedor);
+        precioTotal.innerText="Total: $"+0;
     }
 }
-sinProductos()
+sinProductos();
 
 
 // para que el precio final del carrito este cargado siempre que entremos sin modificar productos
-let precioAPagar=0
+let precioAPagar=0;
 
     const cuantoPago=()=>{
         precioFinal.forEach(e=>{
-            precioAPagar+= e
+            precioAPagar+= e;
         })
-        precioTotal.innerText="Total: $"+precioAPagar
+        precioTotal.innerText="Total: $"+precioAPagar;
 
     }
-    cuantoPago()
+    cuantoPago();
 
 
 //cuando decidimos comprar los productos, se nos habilita la compra con tarjeta   
 comprar.onclick=()=>{
     
     if(productosCarro.length >= 1){
-        bodyCarrito.style.display="none"
-        bodyCard.style.display="flex"
+        bodyCarrito.style.display="none";
+        bodyCard.style.display="flex";
     }
   
 }
@@ -193,105 +195,105 @@ comprar.onclick=()=>{
 
 
 // completando datos de tarjeta y validando los inputs
-const nombreTarjeta= document.querySelector("#name")
-const pTarjeta= document.querySelector("#p-tarjeta")
-let validacionNombre= false 
+const nombreTarjeta= document.querySelector("#name");
+const pTarjeta= document.querySelector("#p-tarjeta");
+let validacionNombre= false ;
 nombreTarjeta.oninput=()=>{
     if(nombreTarjeta.value =="" || nombreTarjeta.value.length <2 || nombreTarjeta.value.match( /[0123456789!"#$%&/()?¡@/*-+.]/ )){
-        nombreTarjeta.style.border="1px solid red"
-        pTarjeta.innerText="Nombre Incorrecto"
+        nombreTarjeta.style.border="1px solid red";
+        pTarjeta.innerText="Nombre Incorrecto";
     }else{
-        pTarjeta.innerText = nombreTarjeta.value
-        nombreTarjeta.style.border="1px solid green"
-        validacionNombre=true
+        pTarjeta.innerText = nombreTarjeta.value;
+        nombreTarjeta.style.border="1px solid green";
+        validacionNombre=true;
     }
     
 } 
 
 
-const apellidoTarjeta= document.querySelector("#surname")
-const pApellido=document.querySelector("#p-apellido")
-let validacionApellido= false
+const apellidoTarjeta= document.querySelector("#surname");
+const pApellido=document.querySelector("#p-apellido");
+let validacionApellido= false;
 apellidoTarjeta.oninput=()=>{
     if(apellidoTarjeta.value =="" || apellidoTarjeta.value.length <2 || apellidoTarjeta.value.match( /[0123456789!"#$%&/()?¡@/*-+.]/ )){
-        apellidoTarjeta.style.border="1px solid red"
-        pApellido.innerText="Nombre Incorrecto"
+        apellidoTarjeta.style.border="1px solid red";
+        pApellido.innerText="Nombre Incorrecto";
     }else{
-        pApellido.innerText = apellidoTarjeta.value
-        apellidoTarjeta.style.border="1px solid green"
-        validacionApellido=true
+        pApellido.innerText = apellidoTarjeta.value;
+        apellidoTarjeta.style.border="1px solid green";
+        validacionApellido=true;
     }
   
 } 
 
 
-const numeroTarjeta= document.querySelector("#numero-tarjeta")
-const pNumero= document.querySelector("#p-numero")
-let validacionNumero= false
+const numeroTarjeta= document.querySelector("#numero-tarjeta");
+const pNumero= document.querySelector("#p-numero");
+let validacionNumero= false;
 numeroTarjeta.oninput=()=>{
     if(numeroTarjeta.value=="" || numeroTarjeta.value.length >16){
-        numeroTarjeta.style.border="1px solid red"
-        pNumero.innerText="Numero Incorrecto"
+        numeroTarjeta.style.border="1px solid red";
+        pNumero.innerText="Numero Incorrecto";
     }else{
-        pNumero.innerText=espaciar()
-        numeroTarjeta.style.border="1px solid green"
-        validacionNumero=true
+        pNumero.innerText=espaciar();
+        numeroTarjeta.style.border="1px solid green";
+        validacionNumero=true;
     }   
 }
 const espaciar=()=>{
-    const espaciado=[...numeroTarjeta.value]
-    espaciado.splice(4,0," ")
-    espaciado.splice(9,0," ")
-    espaciado.splice(14,0," ")
-    const resultado= espaciado.join("")
+    const espaciado=[...numeroTarjeta.value];
+    espaciado.splice(4,0," ");
+    espaciado.splice(9,0," ");
+    espaciado.splice(14,0," ");
+    const resultado= espaciado.join("");
     return resultado
 }
 
 
-const numeroVencimiento= document.querySelector("#vencimiento")
-const pVencimiento= document.querySelector("#p-vencimiento")
-let validacionVencimiento=false
+const numeroVencimiento= document.querySelector("#vencimiento");
+const pVencimiento= document.querySelector("#p-vencimiento");
+let validacionVencimiento=false;
 
 //validacion mes y año, para poder validar el input fecha de vencimiento
-const anioActual = String (new Date().getFullYear())
-const anio= anioActual.slice(2)
-let mesActual= String (new Date().getMonth())
+const anioActual = String (new Date().getFullYear());
+const anio= anioActual.slice(2);
+let mesActual= String (new Date().getMonth());
 
 numeroVencimiento.oninput=()=>{
     if(numeroVencimiento.value.slice(0,2) < mesActual || numeroVencimiento.value.slice(0,2) >12){
-        pVencimiento.innerText="Fecha incorrecta"       
+        pVencimiento.innerText="Fecha incorrecta"    ;   
     } else if(numeroVencimiento.value.slice(2) < anio){
-        pVencimiento.textContent="Fecha incorrecta"
+        pVencimiento.textContent="Fecha incorrecta";
     }else{
-        pVencimiento.innerText=agregarBarra()
-        validacionVencimiento=true
+        pVencimiento.innerText=agregarBarra();
+        validacionVencimiento=true;
     }
 }
 const agregarBarra= ()=>{
-    const separado=[...numeroVencimiento.value]
-    separado.splice(2,0,"/")
-    const resultado= separado.join("")
+    const separado=[...numeroVencimiento.value];
+    separado.splice(2,0,"/");
+    const resultado= separado.join("");
     return resultado
 }
 
 
-const codigoSeguridad= document.querySelector("#cvv")
-const pCodigoSeguridad= document.querySelector("#p-cvv")
-let validacionCvv= false
+const codigoSeguridad= document.querySelector("#cvv");
+const pCodigoSeguridad= document.querySelector("#p-cvv");
+let validacionCvv= false;
 codigoSeguridad.oninput=()=>{
     if(codigoSeguridad.value.length <3 || codigoSeguridad.value.length >3){
-        pCodigoSeguridad.textContent="CVV Incorrecto"
+        pCodigoSeguridad.textContent="CVV Incorrecto";
     }else{
-        pCodigoSeguridad.innerText=codigoSeguridad.value
-        validacionCvv=true
+        pCodigoSeguridad.innerText=codigoSeguridad.value;
+        validacionCvv=true;
     }
 }
 
 
 // si todos los datos son correctos, realizamos la compra correctamente, borrando y modificando todos los articulos que anteriormente se encontraban en el carrito
-const compraRealizada= document.querySelector("#compra-realizada")
+const compraRealizada= document.querySelector("#compra-realizada");
 compraRealizada.onclick=()=>{
-    datosCorrectos()  
+    datosCorrectos();  
 }
 
 const datosCorrectos=()=>{
@@ -302,35 +304,35 @@ const datosCorrectos=()=>{
         icon: 'success',
         confirmButtonText: 'Aceptar'
         })
-        cuantosProductos.innerText="0"
-        bodyCarrito.style.display="block"
-        bodyCard.style.display="none"
-        registrarLocalStorage("carro",[])
-        contenedorJs.innerHTML=""
-        localStorage.setItem("cuantos",0)
-        sinProductos()
+        cuantosProductos.innerText="0";
+        bodyCarrito.style.display="block";
+        bodyCard.style.display="none";
+        registrarLocalStorage("carro",[]);
+        contenedorJs.innerHTML="";
+        localStorage.setItem("cuantos",0);
+        sinProductos();
     }
 }
 
 // tambien agregamos opcion y validacion por si el cliente decide pagar en efectivo
 
-const containerEft=document.querySelector(".container-eft")
-const pagoEnEfectivo= document.querySelector("#pago-efectivo")
+const containerEft=document.querySelector(".container-eft");
+const pagoEnEfectivo= document.querySelector("#pago-efectivo");
 
 pagoEnEfectivo.onclick=()=>{
-    bodyCard.style.display="none"
-    containerEft.style.display="flex"
+    bodyCard.style.display="none";
+    containerEft.style.display="flex";
 }
 
 const cerrarEft= document.querySelector("#close-eft")
 cerrarEft.onclick=()=>{
-    containerEft.style.display="none"
-    bodyCard.style.display="none"
-    bodyCarrito.style.display="block"
+    containerEft.style.display="none";
+    bodyCard.style.display="none";
+    bodyCarrito.style.display="block";
 }
 
-const inputEmail=document.querySelector("#input-email")
-const enviarCupon= document.querySelector("#enviar-cupon")
+const inputEmail=document.querySelector("#input-email");
+const enviarCupon= document.querySelector("#enviar-cupon");
 enviarCupon.onclick=()=>{
 
     if( inputEmail.value.length >5 && inputEmail.value.match(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i) ){
@@ -340,43 +342,30 @@ enviarCupon.onclick=()=>{
     icon: 'success',
     confirmButtonText: 'Aceptar'
     })
-    containerEft.style.display="none"
-    bodyCard.style.display="none"
-    bodyCarrito.style.display="block"
-    registrarLocalStorage("carro",[])
-    contenedorJs.innerHTML=""
-    localStorage.setItem("cuantos",0)
+    containerEft.style.display="none";
+    bodyCard.style.display="none";
+    bodyCarrito.style.display="block";
+    registrarLocalStorage("carro",[]);
+    contenedorJs.innerHTML="";
+    localStorage.setItem("cuantos",0);
     sinProductos()
     }else{
-        inputEmail.style.border="1px solid red"
+        inputEmail.style.border="1px solid red";
     }
 
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 cerrar.onclick=()=>{
-    bodyCarrito.style.display="block"
-    bodyCard.style.display="none"
+    bodyCarrito.style.display="block";
+    bodyCard.style.display="none";
 }
 
 const comprobar = ()=>{
-    cuantosProductos.innerText=comprobarLocalStorage("cuantos")
+    cuantosProductos.innerText=comprobarLocalStorage("cuantos");
 }
-comprobar()
+comprobar();
 
 
 
--- js/mainContacto.js
+++ js/mainContacto.js
@@ -1,55 +1,55 @@
-const form= document.querySelector("#form-contacto")
-const inputNombre= document.querySelector("#nombre-input")
-const inputApellido= document.querySelector("#apellido-input")
-const inputEmail= document.querySelector("#email-input")
-const inputTelefono= document.querySelector("#telefono-input")
-const inputDudas= document.querySelector("#dudas-input")
+const form= document.querySelector("#form-contacto");
+const inputNombre= document.querySelector("#nombre-input");
+const inputApellido= document.querySelector("#apellido-input");
+const inputEmail= document.querySelector("#email-input");
+const inputTelefono= document.querySelector("#telefono-input");
+const inputDudas= document.querySelector("#dudas-input");
 
-const nombreUser = document.querySelector("#nombre-user")
-const cantidadProductos= document.querySelector("#cantidad")
-const precioIndividual= document.querySelector("#precio-individual")
-const cuantosProductos= document.querySelector(".span-carrito")
+const nombreUser = document.querySelector("#nombre-user");
+const cantidadProductos= document.querySelector("#cantidad");
+const precioIndividual= document.querySelector("#precio-individual");
+const cuantosProductos= document.querySelector(".span-carrito");
 
-const totalFinal= document.querySelectorAll("#total")
-const interruptor= document.querySelector(".interruptor")
+const totalFinal= document.querySelectorAll("#total");
+const interruptor= document.querySelector(".interruptor");
 const aside=document.querySelector(".aside");
-const main=document.querySelector(".main")
+const main=document.querySelector(".main");
 const body=document.querySelector("body");
-const modoCheckbox= document.querySelector("#toggle")
-const contenedorJs= document.querySelector("#container-js")
-const vaciar= document.querySelector(".vaciar")
-const precioTotal = document.querySelector("#total-final")
+const modoCheckbox= document.querySelector("#toggle");
+const contenedorJs= document.querySelector("#container-js");
+const vaciar= document.querySelector(".vaciar");
+const precioTotal = document.querySelector("#total-final");
 
 
 
 
 
 const registrarLocalStorage = (clave,valor) =>{
-    localStorage.setItem(clave,JSON.stringify(valor))
+    localStorage.setItem(clave,JSON.stringify(valor));
 }
 
 
 const comprobarLocalStorage = ( clave ) => {
-    return JSON.parse(localStorage.getItem(clave))
+    return JSON.parse(localStorage.getItem(clave));
 }
 
 
 //validar nombre de usuario que inicio sesion
 const cargarCarrito=()=>{
     document.body.onload=()=>{
-        const nombre= comprobarLocalStorage("nombre")      
-        nombreUser.textContent= nombre
+        const nombre= comprobarLocalStorage("nombre")    ;  
+        nombreUser.textContent= nombre;
     }
 }
-cargarCarrito()
+cargarCarrito();
 
-
+// validaciones por separado de cada input, si todas las validaciones son correctas, seran true, y si todo el formulario es true, entonces se envia la informacion
 form.onsubmit=(e)=>{
-     e.preventDefault()
-    validacionNames()
-    validacionEmail()
-    validacionTelefono()
-    validacionDudas()
+     e.preventDefault();
+    validacionNames();
+    validacionEmail();
+    validacionTelefono();
+    validacionDudas();
    if (validacionNames() && validacionEmail() && validacionTelefono() && validacionDudas()){
     Swal.fire({
         title: 'Gracias por enviarnos tus datos.',
@@ -57,69 +57,68 @@ form.onsubmit=(e)=>{
         icon: 'success',
         confirmButtonText: 'Aceptar'
       })
-    inputNombre.value=""
-    inputApellido.value=""
-    inputEmail.value=""
-    inputTelefono.value=""
-    inputDudas.value=""
-    inputNombre.style.border="1px solid lightgrey"
-    inputApellido.style.border="1px solid lightgrey"
-    inputEmail.style.border="1px solid lightgrey"
-    inputTelefono.style.border="1px solid lightgrey"
-    inputDudas.style.border="1px solid lightgrey"
+    inputNombre.value="";
+    inputApellido.value="";
+    inputEmail.value="";
+    inputTelefono.value="";
+    inputDudas.value="";
+    inputNombre.style.border="1px solid lightgrey";
+    inputApellido.style.border="1px solid lightgrey";
+    inputEmail.style.border="1px solid lightgrey";
+    inputTelefono.style.border="1px solid lightgrey";
+    inputDudas.style.border="1px solid lightgrey";
    }
 }
 
 const validacionNames= ()=>{
     if (inputNombre.value==""){
-        inputNombre.style.border="1px solid red"
+        inputNombre.style.border="1px solid red";
     }else if( inputApellido.value ==""){
-        inputApellido.style.border="1px solid red"
+        inputApellido.style.border="1px solid red";
     }
     else {
-        inputNombre.style.border="1px solid green"
-        inputApellido.style.border="1px solid green"
+        inputNombre.style.border="1px solid green";
+        inputApellido.style.border="1px solid green";
         return true
     }
 }
 
 const validacionEmail=()=>{
     if(inputEmail.value.match(/^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i)){
-        inputEmail.style.border="1px solid green"   
+        inputEmail.style.border="1px solid green";  
         return true
     }else {
-        inputEmail.style.border="1px solid red"
+        inputEmail.style.border="1px solid red";
     }
 }
 
 const validacionTelefono=()=>{
     if( (inputTelefono.value.length>=8) && (inputTelefono.value.match( /^[0-9]+$/)) ){
-        inputTelefono.style.border="1px solid green"
+        inputTelefono.style.border="1px solid green";
         return true
     }else {
-        inputTelefono.style.border="1px solid red"
+        inputTelefono.style.border="1px solid red";
     }
 }
 
 const validacionDudas=()=>{
     if (inputDudas.value.length>=4){
-        inputDudas.style.border="1px solid green"
+        inputDudas.style.border="1px solid green";
         return true
     }else{
-        inputDudas.style.border="1px solid red"
+        inputDudas.style.border="1px solid red";
     }
 }
 
 
 // modo oscuro
 interruptor.onclick= ()=>{
-    // productosTitulo.classList.toggle("modo-oscuro")
-    aside.classList.toggle("modo-oscuro")
-    main.classList.toggle("modo-oscuro-main")
-    body.classList.toggle("modo-oscuro")
-    registrarLocalStorage("modo","oscuro")
+    aside.classList.toggle("modo-oscuro");
+    main.classList.toggle("modo-oscuro-main");
+    body.classList.toggle("modo-oscuro");
+    registrarLocalStorage("modo","oscuro");
     if(modoCheckbox.checked==true){
-        localStorage.removeItem("modo")
+        localStorage.removeItem("modo");
     }
 }
 
@@ -127,27 +126,25 @@ interruptor.onclick= ()=>{
 
 function validarModoOscuro(clave){
     if  (clave=="oscuro"){
-        // productosTitulo.classList.add("modo-oscuro")
-        aside.classList.add("modo-oscuro")
-        main.classList.add("modo-oscuro-main")
-        body.classList.add("modo-oscuro")
-        modoCheckbox.checked=true
+        aside.classList.add("modo-oscuro");
+        main.classList.add("modo-oscuro-main");
+        body.classList.add("modo-oscuro");
+        modoCheckbox.checked=true;
 
     }else {
-        // productosTitulo.classList.remove("modo-oscuro")
-        aside.classList.remove("modo-oscuro")
-        main.classList.remove("modo-oscuro-main")
-        body.classList.remove("modo-oscuro")
-        modoCheckbox.checked=false
+        aside.classList.remove("modo-oscuro");
+        main.classList.remove("modo-oscuro-main");
+        body.classList.remove("modo-oscuro");
+        modoCheckbox.checked=false;
     }
 
 
 }
 
-validarModoOscuro(comprobarLocalStorage("modo"))
+validarModoOscuro(comprobarLocalStorage("modo"));
 
 
 const comprobar = ()=>{
-    cuantosProductos.innerText=comprobarLocalStorage("cuantos")
+    cuantosProductos.innerText=comprobarLocalStorage("cuantos");
 }
-comprobar()
+comprobar();
